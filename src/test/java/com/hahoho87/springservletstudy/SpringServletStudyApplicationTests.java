3c2136576e0b904732ab35f8c8868687803b6123
hahoho87 <hahoho87@gmail.com>
2021-05-26 08:55:16 +0900

    Initial commit

---
+++ src/test/java/com/hahoho87/springservletstudy/SpringServletStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hahoho87.springservletstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServletStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
