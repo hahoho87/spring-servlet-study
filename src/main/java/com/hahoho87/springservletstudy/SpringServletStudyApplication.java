36df6811c5802ae4fd631246cd56df9607be843c
hahoho87 <hahoho87@gmail.com>
2021-05-26 12:02:02 +0900

    basic 서블릿 추가

--- src/main/java/com/hahoho87/springservletstudy/SpringServletStudyApplication.java
+++ src/main/java/com/hahoho87/springservletstudy/SpringServletStudyApplication.java
@@ -2,8 +2,10 @@ package com.hahoho87.springservletstudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class SpringServletStudyApplication {
 
     public static void main(String[] args) {
